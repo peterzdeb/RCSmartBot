ae83694ce0e598b3550ad37be4c8c4422de7b82f
Petro Zdeb <peterzdeb@gmail.com>
2017-03-11 13:32:36 +0200

    Fixed dist following strategy to work on steerig motor driver

---
+++ strategies/base_strategy.py
@@ -0,0 +1,62 @@
+import asyncio
+
+
+class BaseRobotStrategy(object):
+
+    def __init__(self):
+        self.active_actions = {}
+        self.log_data = {}
+
+    def on_stop(self, active=True):
+        pass
+
+    def on_forward(self, active=True):
+        pass
+
+    def on_backward(self, active=True):
+        pass
+
+    def on_left(self, active=True):
+        pass
+
+    def on_right(self, active=True):
+        pass
+
+    @asyncio.coroutine
+    def sensor_handler(self, name, data):
+        print('WARNING NotImplemented: Sensor "%s", data=%s' % (name, data))
+
+    @asyncio.coroutine
+    def event_handler(self, **event_data):
+        """ Must be implemented for each strategy """
+        #TODO: move into WebGamepad package
+        action = event_data.get('action')
+        action_type = event_data.get('type')
+
+        action_event = None
+        if action == 'up':
+            action_event = self.on_forward
+        elif action == 'down':
+            action_event = self.on_backward
+        elif action == 'right':
+            action_event = self.on_right
+        elif action == 'left':
+            action_event = self.on_left
+
+        if not action_event:
+            return self.active_actions
+        if action_type == 'key_down':
+            self.active_actions[action] = 1
+            yield from action_event(active=True)
+        elif action_type == 'key_up':
+            if action in self.active_actions:
+                del self.active_actions[action]
+                yield from action_event(active=False)
+        print('keys: %s' % self.active_actions.keys())
+        return self.active_actions
+
+    @asyncio.coroutine
+    def log(self):
+#        print('store_logs: %s' % self.__log_data)
+        data = ','.join(map(str, self.log_data.get('dists', [0,0,0])))
+        data += ',' + self.log_data.get('action', '') + "\n"
---
+++ strategies/motorized_robot.py
@@ -0,0 +1,133 @@
+import asyncio
+
+from motor_engine.motor_driver import MotorDriver, DualMotorDriver
+from motor_engine.servo_driver import ServoDriver
+from .base_strategy import BaseRobotStrategy
+
+
+class DualMotorRobotStrategy(BaseRobotStrategy):
+
+    def __init__(self, log=None):
+        super(DualMotorRobotStrategy, self).__init__()
+        self.__log_data = {}
+        self.__locked = False
+        self.__moving = False
+        self.__log = log
+        self.distances = {}
+
+        motor_a = MotorDriver(16, 18, 22)
+        motor_b = MotorDriver(19, 21, 23)
+        self.motor = DualMotorDriver(motor_a, motor_b)
+
+    @asyncio.coroutine
+    def on_forward(self, active=True):
+        if active:
+            self.__log_data['action'] = 'down_start'
+            self.motor.forward()
+        else:
+            yield from self.on_stop()
+
+    @asyncio.coroutine
+    def on_backward(self, active=True):
+        if active:
+            self.motor.backward()
+        else:
+            yield from self.on_stop()
+
+    @asyncio.coroutine
+    def on_left(self, active=True):
+        if active:
+            self.motor.turn_left()
+        else:
+            if self.motor.is_forward:
+                yield from self.on_forward()
+            elif self.motor.is_backward:
+                yield from self.on_backward()
+            else:
+                yield from self.on_stop()
+
+    @asyncio.coroutine
+    def on_right(self, active=True):
+        if active:
+            self.motor.turn_right()
+        else:
+            if self.motor.is_forward:
+                yield from self.on_forward()
+            elif self.motor.is_backward:
+                yield from self.on_backward()
+            else:
+                yield from self.on_stop()
+
+    @asyncio.coroutine
+    def on_stop(self, active=True):
+        if not self.motor.stopped:
+            self.__log_data['action'] = 'down_end'
+            self.motor.stop()
+
+
+class MotorizedSteeringRobotStrategy(BaseRobotStrategy):
+
+    def __init__(self, log=None):
+        super(MotorizedSteeringRobotStrategy, self).__init__()
+        self.loop = asyncio.get_event_loop()
+        self.initial_step = 30
+        self.steering_tick = 0.1
+
+        self.turning_left = False
+        self.turning_right = False
+
+        self.motor = MotorDriver(16, 18, 22)
+        self.steer = ServoDriver(0)
+
+    @asyncio.coroutine
+    def on_forward(self, active=True):
+        if active:
+            self.log_data['action'] = 'down_start'
+            self.motor.forward()
+        else:
+            yield from self.on_stop()
+
+    @asyncio.coroutine
+    def on_backward(self, active=True):
+        if active:
+            self.motor.backward()
+        else:
+            yield from self.on_stop()
+
+    @asyncio.coroutine
+    def on_left(self, active=True):
+        if active:
+            self.turning_left = True
+            self.loop.create_task(self.steer_left())
+        else:
+            self.turning_left = False
+
+    @asyncio.coroutine
+    def on_right(self, active=True):
+        if active:
+            self.turning_right = True
+            self.loop.create_task(self.steer_right())
+        else:
+            self.turning_right = False
+
+    @asyncio.coroutine
+    def on_stop(self):
+        if not self.motor.stopped:
+            self.log_data['action'] = 'down_end'
+            self.motor.stop()
+
+    @asyncio.coroutine
+    def steer_left(self):
+        step = self.initial_step
+        while self.turning_left:
+            self.steer.turn_left(step)
+            #step += step
+            yield from asyncio.sleep(self.steering_tick)
+
+    @asyncio.coroutine
+    def steer_right(self):
+        step = self.initial_step
+        while self.turning_right:
+            self.steer.turn_right(step)
+            #step += step
+            yield from asyncio.sleep(self.steering_tick)
